bb81e18f2c0d9d55e8dbdb83575e51e9c99a3b72
Kseniya <baharevaxen@yandex.ru>
2026-04-17 19:03:42 +0300

    correcting task

--- booking/tasks.py
+++ booking/tasks.py
@@ -1,7 +1,7 @@
 from celery import shared_task
 from django.core.mail import send_mail
 from config import settings
-
+from django.conf import settings
 
 @shared_task
 def send_reservation_confirmation_email(user_email, table, date, time):
