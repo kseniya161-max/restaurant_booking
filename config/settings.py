bf0b1b91bd60b21308e87a70b9970ec1fb96f4b4
Kseniya <baharevaxen@yandex.ru>
2026-04-17 15:10:10 +0300

    adding celery settings

--- config/settings.py
+++ config/settings.py
@@ -119,3 +119,6 @@ EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+
+CELERY_BROKER_URL = os.getenv("CELERY_BROKER_URL", "redis://localhost:6379/0")
+CELERY_RESULT_BACKEND = os.getenv("CELERY_RESULT_BACKEND", "redis://localhost:6379/0")
